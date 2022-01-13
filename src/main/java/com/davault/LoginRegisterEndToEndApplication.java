6cd96063e7e0bfbc9f46abe6aaa1f40b19eeedc4
Joe Davault <joe.davault@gmail.com>
2022-01-12 18:35:06 -0700

    first commit

---
+++ src/main/java/com/davault/LoginRegisterEndToEndApplication.java
@@ -0,0 +1,13 @@
+package com.davault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegisterEndToEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginRegisterEndToEndApplication.class, args);
+    }
+
+}
